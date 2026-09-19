1240529fc82825edb94f4cb4c1c7ad2f0ce3711f
Isaac Leite <isaacbonvivan@gmail.com>
2026-09-19 17:10:56 -0300

    style: strengthen notification close icon

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -20,7 +20,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "!border-l-4 !pr-12 [&[data-type=success]]:!bg-[#effcf3] [&[data-type=success]]:!text-[#237a45] [&[data-type=success]]:!border-[#bbf7d0] [&[data-type=success]]:!border-l-[#22c55e] [&[data-type=success]_[data-title]]:!text-[#237a45] [&_[data-icon]]:!text-green-600 [&_[data-close-button]]:!text-green-600",
           description: "group-[.toast]:text-muted-foreground",
           closeButton:
-            "!left-auto !right-3 !top-1/2 !h-9 !w-9 !-translate-y-1/2 !translate-x-0 !border-0 !bg-transparent !text-slate-500 hover:!bg-black/5 hover:!text-slate-800 [&>svg]:!h-4 [&>svg]:!w-4",
+            "!left-auto !right-3 !top-1/2 !h-9 !w-9 !-translate-y-1/2 !translate-x-0 !border-0 !bg-transparent !text-slate-500 hover:!bg-black/5 hover:!text-slate-800 [&>svg]:!h-4 [&>svg]:!w-4 [&>svg]:!stroke-2",
           actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
